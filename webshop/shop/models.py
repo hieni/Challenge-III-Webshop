9f02e2c26d962cd63f4abccff5d66652e2d7a5aa
unknown <v.thach@stud.bhh.de>
2025-12-02 23:02:41 +0100

    Login Page mit AI zum laufen bekommen, also nochmal drüber schauen und ggf. anpassen. z.B. in base.html ist die Navbar noch etwas unschön

--- webshop/shop/models.py
+++ webshop/shop/models.py
@@ -1,6 +1,6 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
 
-# Create your models here.
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -9,6 +9,15 @@ class Customer(models.Model):
     strasse = models.CharField(max_length=100)
     ort = models.CharField(max_length=50)
     plz = models.CharField(max_length=10)
+    
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50)
--- webshop/shop/urls.py
+++ webshop/shop/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.product_list, name='product_list'),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]

a4255ac67c18ba6839dba57da8fb2e81ca49f85e
unknown <v.thach@stud.bhh.de>
2025-12-03 14:16:53 +0100

    Warenkorb und zum Warenkorb hinzufügen funktioniert. Siehe urls, views und templates

--- webshop/shop/urls.py
+++ webshop/shop/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('products/', views.product_list, name='product_list'),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+    path("add_to_cart/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
+    path("cart/", views.cart_view, name="cart"),
 ]
--- webshop/shop/views.py
+++ webshop/shop/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from .models import Product, Customer
+from .models import Product, Customer, Cart, CartItem
 
 def home(request):
     return render(request, "home.html")
@@ -35,3 +35,48 @@ def logout_view(request):
     request.session.flush()
     messages.info(request, "Du wurdest ausgeloggt.")
     return redirect("login")
+
+def add_to_cart(request, product_id):
+    customer_id = request.session.get("customer_id")
+    if not customer_id:
+        messages.error(request, "Bitte logge dich ein, um Artikel zum Warenkorb hinzuzufügen.")
+        return redirect("product_list")
+
+    customer = Customer.objects.get(id=customer_id)
+    product = get_object_or_404(Product, id=product_id)
+
+    cart, _ = Cart.objects.get_or_create(customer=customer)
+
+    cart_item, item_created = CartItem.objects.get_or_create(
+        cart=cart,
+        product=product,
+        defaults={"quantity": 1}
+    )
+
+    if not item_created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    messages.success(request, f"{product.name} wurde in den Warenkorb gelegt.")
+    return redirect("product_list")
+
+def cart_view(request):
+    customer_id = request.session.get("customer_id")
+    if not customer_id:
+        messages.error(request, "Bitte logge dich ein, um deinen Warenkorb zu sehen.")
+        return redirect("product_list")
+
+    customer = Customer.objects.get(id=customer_id)
+    cart, _ = Cart.objects.get_or_create(customer=customer)
+    cart_items = CartItem.objects.filter(cart=cart)
+
+    for item in cart_items:
+        item.total_price = item.product.price * item.quantity
+
+    total_price = sum(item.total_price for item in cart_items)
+
+    context = {
+        "cart_items": cart_items,
+        "total_price": total_price,
+    }
+    return render(request, "cart.html", context)
